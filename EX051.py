092dc053be8c646928fbf0aa8e1d2412d3d5b390
MrJuninn <reginaldorodrigues8838@gmail.com>
2024-08-13 08:17:11 -0300

    Detalhes no final

--- EX051.py
+++ EX051.py
@@ -1,7 +1,8 @@
 # Valores para a PA
 pa = int(input('Primeiro Termo: '))
 raz = int(input('Razão: '))
+décimo = pa + (10 - 1) * raz
 # Lógica para a PA
-for c in range(pa, 10*raz, raz):
+for c in range(pa, décimo + raz, raz):
     print(c, end=(' - '))
 print('Acabou')
