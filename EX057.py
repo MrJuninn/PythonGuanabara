a84eba9920576beedc2f58dcc72b42e304969759
MrJuninn <reginaldorodrigues8838@gmail.com>
2024-08-16 07:11:29 -0300

    Avante

--- EX057.py
+++ EX057.py
@@ -1,6 +1,6 @@
 '''sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo = str(input('Qual o seu sexo? [M/F]:')).upper()
+    sexo = str(input('Qual o seu sexo? [M/F]:')).upper().strip()[0]
     if sexo != 'M' and sexo != 'F':
         print('\033[31mValor Invalido, Tente Novamente\033[m')
     if sexo == 'M':
@@ -10,15 +10,22 @@ while sexo != 'M' and sexo != 'F':
 print('Acabou')'''
 # Minha resolução ↑
 
-sexo = ''
+'''sexo = ''
 while sexo != 'M' and sexo != 'F':
-    sexo =  str(input('Informe seu sexo: [M/F] ')).upper()
+    sexo =  str(input('Informe seu sexo: [M/F] ')).upper().strip()[0]
     if sexo != 'M' and sexo != 'F':
         while sexo != 'M' and sexo != 'F':
-            sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).upper()
+            sexo = str(input('Dados inválidos. Por favor, informe seu sexo: ')).upper().strip()[0]
     if sexo == 'M':
             print('Sexo M registrado com sucesso')
     if sexo == 'F':
-            print('Sexo F registrado com sucesso')
+            print('Sexo F registrado com sucesso')'''
 
 # Fiz sozin :b ambos
+
+# guana resolução ▼
+
+sexo = str(input('Informe seu sexo: [M/F] ')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dados Inválidos. Por favor, Informe seu sexo: ')).strip().upper()[0]
+print('Sexo {} registrado com sucesso'.format(sexo))
