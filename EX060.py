e3e8aa6e60c5453539d7bd819b2fc552e329ba4e
MrJuninn <reginaldorodrigues8838@gmail.com>
2024-08-17 06:10:43 -0300

    Ajustes

--- EX060.py
+++ EX060.py
@@ -1,7 +1,13 @@
-fatorial = int(input('Digite um número: '))
+fatorial = int(input('Digite um número para \ncalcular seu Fatorial: '))
 fatorialc = 1
+print('Calculando {}! = '.format(fatorial), end='')
 while fatorial != 0:
-    print('{}x'.format(fatorial),end='')
-    fatorialc = fatorialc * fatorial
-    fatorial = fatorial - 1
-
+    # Sobre o calculo, o que vai ser mostrado e o que vai ser calculado ↓
+    if fatorial != 1:
+        print('{} X '.format(fatorial),end='')               # print('{}'.format(c), end'')
+        fatorialc *= fatorial                    # print(' x ' if c > 1 else ' = ', end='')
+        fatorial -= 1
+    # Sobre o sinal de igual no final ↓
+    if fatorial == 1:
+        print('{} = {}'.format(fatorial, fatorialc))
+        fatorial -= 1
