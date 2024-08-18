0768fb52988022811f6f514bca4e87b8314e4636
MrJuninn <reginaldorodrigues8838@gmail.com>
2024-08-18 11:20:39 -0300

    Trabalho

--- EX066.py
+++ EX066.py
@@ -1,7 +1,6 @@
-cont = 0
-soma = 0
+cont = soma = 0
 while True:
-    n = int(input('Digite um número: '))
+    n = int(input('Digite um número [999 para parar]: '))
     if n == 999:
         break
     cont += 1
