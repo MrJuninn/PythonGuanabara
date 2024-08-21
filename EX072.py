29a0c6d8b68c097b685048e80c2dbb704463d2f4
MrJuninn <reginaldorodrigues8838@gmail.com>
2024-08-21 17:44:04 -0300

    Ajuste

--- EX072.py
+++ EX072.py
@@ -1,10 +1,12 @@
-print('-='*10)
-print('Números Por Extenso')
-print('=-'*10)
+resp = 'S'
 extenso = ('Zero', 'Um', 'Dois', 'Três', 'Quatro', 'Cinco', 'Seis', 'Sete', 'Oito', 'Nove', 'Dez', 'Onze', 'Doze', 'Treze', 'Quatorze', 'Quinze', 'Dezesseis', 'Dezessete', 'Dezoito', 'Dezenove', 'Vinte')
-player = int(input('Digite um valor entre 0 e 20 : '))
-if player > 20 or player < 0:
-    while player > 20 or player < 0:
-        player = int(input('Tente Novamente, Digite um valor entre 0 e 20 : '))
-print('=-'*10)
-print(extenso[player])
+while True:
+    if resp in 'S':
+        player = int(input('Digite um valor entre 0 e 20 : '))
+        while 0 < player > 20:
+            player = int(input('Tente Novamente, Digite um valor entre 0 e 20 : '))
+        print('=-' * 10)
+        print(f'Você digitou o número {extenso[player]}')
+        resp = str(input('Deseja continuar?[S/N] ')).strip().upper()[0]
+    else:
+        break
