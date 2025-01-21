8443112e3fe39f37fec9873ef9a54d5686788c06
MrJuninn <reginaldorodrigues8838@gmail.com>
2025-01-21 19:14:36 -0300

    de volta XD

---
+++ EX098.py
@@ -0,0 +1,26 @@
+from time import sleep
+
+
+def contador(i, f, p):
+    print(f'Contagem de {i} até {f} de {p} em {p}')
+    if p == 0:
+        p = 1
+    if f < i:
+        for c in range(i, f - p, -p):
+            print(f'{c} ', end='')
+            sleep(0.5)
+    if f > i:
+        for c in range(i, f + p, p):
+            print(f'{c} ', end='')
+            sleep(0.5)
+    print('FIM!')
+    print('-=' * 20)
+
+# Programa Principal
+contador(1, 10, 1)
+contador(10, 0, 2)
+print('Agora é sua vez de personalizar a contagem!')
+ini = int(input('Início: '))
+fim = int(input('Fim: '))
+passo = int(input('Passo: '))
+contador(ini, fim, abs(passo))
