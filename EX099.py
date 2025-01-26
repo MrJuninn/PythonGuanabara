c1eb7dd57205c3c432b65e9b5ac841c6fa7b51e8
MrJuninn <reginaldorodrigues8838@gmail.com>
2025-01-26 07:01:44 -0300

    .

---
+++ EX099.py
@@ -0,0 +1,20 @@
+def maior(* nums):
+    cont = mai = 0
+    print('-=' * 28)
+    print('Analisando os valores passados...')
+    for c in nums:
+        cont += 1
+        print(f'{c} ', end='')
+        if c > mai:
+            mai = c
+    print(f'Foram informados {cont} valores ao todo.')
+    print(f'O maior valor informado foi {mai}.')
+
+
+
+# Programa Principal
+maior(2, 9, 4, 5, 7, 1)
+maior(4, 7, 0)
+maior(1, 2)
+maior(6)
+maior()
