12ffd901d33af3bb63e9079144cd30894c1e9f05
MrJuninn <reginaldorodrigues8838@gmail.com>
2025-04-14 16:36:49 -0300

    .

---
+++ Ex107/moeda.py
@@ -0,0 +1,18 @@
+def aumentar(n, tax):
+    res = n + (n * tax / 100)
+    return res
+
+
+def diminuir(n, tax):
+    res = n - (n * tax / 100)
+    return res
+
+
+def dobro(n):
+    res = 2 * n
+    return res
+
+
+def metade(n):
+    res = n / 2
+    return res
